06e4fdde95369083b4d375ad0ae39dd7bcdc1a15
yveso <yveso@users.noreply.github.com>
2018-12-12 21:26:57 +0100

    Day 11

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,27 +1,18 @@
 import React from "react";
 import { unstable_createResource as createResource } from "react-cache";
 
-let PokemonDetailResource = createResource(() =>
-  fetch("https://pokeapi.co/api/v2/pokemon/25/").then(res => res.json())
+let PokemonResource = createResource(id =>
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then(res => res.json())
 );
 
-function PokemonDetail() {
+function PokemonDetail({ pokemonId: id }) {
+  let pokemon = PokemonResource.read(id);
   return (
-    <div>
-      {[PokemonDetailResource.read()].map(item => (
-        <PokemonDetailComponent key={item.name} {...item} />
-      ))}
-    </div>
+    <article>
+      <h1>It's {pokemon.name}</h1>
+      <img src={pokemon.sprites["front_default"]} alt={pokemon.name} />
+    </article>
   );
 }
 
-function PokemonDetailComponent(props) {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>
-        Weight: <strong>{props.weight}</strong>
-      </p>
-    </div>
-  );
-}
+export default PokemonDetail;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { unstable_createResource as createResource } from "react-cache";
 import ErrorBoundary from "./ErrorBoundary";
+import PokemonDetail from "./PokemonDetail";
 
 let PokemonCollectionResource = createResource(() =>
   fetch("https://pokeapi.co/api/v2/pokemon/").then(res => res.json())
@@ -17,24 +18,19 @@ function PokemonListItem({ className, component: Component = "li", ...props }) {
 }
 
 function PokemonList({ renderItem }) {
-  return (
-    <ul>
-      {PokemonCollectionResource.read().results.map(pokemon =>
-        renderItem({ id: pokemon.url.split("/")[6], ...pokemon })
-      )}
-    </ul>
+  return PokemonCollectionResource.read().results.map(pokemon =>
+    renderItem({ id: pokemon.url.split("/")[6], ...pokemon })
   );
 }
 
 function App() {
-  let [selectedPokemonId, setSelectedPokemonID] = useState(0);
+  let [selectedPokemonId, setSelectedPokemonID] = useState(1);
 
   return (
     <div>
-      <h1>React Holiday 2018: Day 10</h1>
+      <h1>React Holiday 2018: Day 11</h1>
       <hr />
-      <strong>Selected Pokemon ID: {selectedPokemonId}</strong>
-      <hr />
+
       <ErrorBoundary
         fallback={
           <div>
@@ -45,17 +41,24 @@ function App() {
           </div>
         }
       >
+        <React.Suspense fallback={<div>...loading details</div>}>
+          <PokemonDetail pokemonId={selectedPokemonId} />
+        </React.Suspense>
+
         <React.Suspense fallback={<div>...loading</div>}>
-          <PokemonList
-            renderItem={pokemon => (
-              <PokemonListItem
-                onClick={() => setSelectedPokemonID(pokemon.id)}
-                key={pokemon.id}
-              >
-                {pokemon.name}
-              </PokemonListItem>
-            )}
-          />
+          <hr />
+          <ul>
+            <PokemonList
+              renderItem={pokemon => (
+                <PokemonListItem
+                  onClick={() => setSelectedPokemonID(pokemon.id)}
+                  key={pokemon.id}
+                >
+                  {pokemon.name}
+                </PokemonListItem>
+              )}
+            />
+          </ul>
         </React.Suspense>
       </ErrorBoundary>
     </div>
